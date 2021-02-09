7c374f1ac14845e19d2324deeec3cad146bbf4f8
Anthony Grooms <48000635+anthonygrooms01@users.noreply.github.com>
2021-02-09 18:10:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,68 +1,79 @@
-import random
-
-"""
+'''
+    Blueprint:
 
     Pick a random word from the word list
-    
     Ask player to enter a letter
-
     Everytime the player enters a letter that is in the word, reveal that letter
-
     Process continues until all letters are revealed or the player ran out of guesses
 
-"""
+'''
 
-wordsFile = open("Words.txt","r") #open the words file
+import random #import random functionality
+wordsFile = open('Words.txt','r') #open the words file
 lstWords = wordsFile.readlines() #create a list of words
 wordsFile.close() #close the file
-word = lstWords[random.randint(0,len(lstWords)-1)] #pick a random word
-word = word[:-1] #remove the newline from the word
+validLetters = 'abcdefghijklmnopqrstuvwxyz'
 
-#create the hidden word string
-displayWord = ""
-for letter in word:
-    displayWord+="_"
+#run the game indefinitely until the user does not want to play again
+while True:
 
-deadGuyPicture = ["\nO O","\n[.]","\n---","\n | ","\n | ","\n/"," \\"] #load each part of the death picture into the deadGuyPicture list
-guessCount = 0
+    word = lstWords[random.randint(0,len(lstWords)-1)] #pick a random word
+    word = word[:-1] #remove the newline from the word
 
-#Keep track of the letters the user already entered
-usedLetters = []
+    #create the hidden word string
+    displayWord = ''
+    for letter in word:
+        displayWord+='_'
 
-#keep asking for input while the player has not uncovered the whole word and they have used less than 7 guesses
-while displayWord != word and guessCount < 7:
-    displayWordText = "Word: "
+    deadGuyPicture = ['\nO O','\n[.]','\n---','\n | ','\n | ','\n/',' \\'] #load each part of the death picture into the deadGuyPicture list
+    guessCount = 0
 
-    #attach the hidden word string to the hidden word display 
-    for letter in displayWord:
-        displayWordText+=letter+" "
+    #keep track of the letters the user already entered
+    usedLetters = []
 
-    #print the hidden word display
-    print(displayWordText)
+    #keep asking for input while the player has not uncovered the whole word and they have used less than 7 guesses
+    while displayWord != word and guessCount < 7:
 
-    #ask the user for a letter
-    while True:
-        letter = input("Enter letter: ").lower()
-        if letter not in usedLetters:
-            usedLetters.append(letter)
-            break
-        print('You arleady used that letter!')
+        displayWordText = 'Word: '
 
-    #if the letter is in word, edit the hidden word string to display the found letter
-    if letter in word:
-        for index in range(len(word)):
-            if word[index]==letter:
-                displayWord = displayWord[:index]+letter+displayWord[index+1:]
-    else: #otherwise, increase the guesscount, and display guessCount (the amount of incorrect guesses the player used) parts of the death picture
-        guessCount += 1
-        for index in range(guessCount):
-            print(deadGuyPicture[index],end="")
+        #attach the hidden word string to the hidden word display 
+        for letter in displayWord:
+            displayWordText+=letter+' '
+
+        #print the hidden word display
+        print(displayWordText)
+
+        #ask the user for a letter
+        while True:
+            letter = input('Enter letter: ').lower()
+
+            if letter in validLetters:
+                if letter not in usedLetters:
+                    usedLetters.append(letter)
+                    break
+                print('You arleady used that letter!\n')
+            else:
+                print('Not a valid letter\n')
+
+        #if the letter is in word, edit the hidden word string to display the found letter
+        if letter in word:
+            for index in range(len(word)):
+                if word[index]==letter:
+                    displayWord = displayWord[:index]+letter+displayWord[index+1:]
+        else: #otherwise, increase the guesscount, and display guessCount (the amount of incorrect guesses the player used) parts of the death picture
+            guessCount += 1
+            for index in range(guessCount):
+                print(deadGuyPicture[index],end='')
+            print()
+
         print()
 
-    print()
+    #if the guesscount is less than seven, say the player won, otherwise, say the player lossed
+    if guessCount < 7:
+        print('You win! The word is {}. You guessed incorrectly {} {}.'.format(displayWord,guessCount,'time'+('','s')[guessCount!=1]))
+    else:
+        print('You lose! The word was '+word+'.')
 
-#if the guesscount is less than seven, say the player won, otherwise, say the player lossed
-if guessCount < 7:
-    print("You win! The word is {}. You guessed incorrectly {} {}.".format(displayWord,guessCount,'time'+('s','')[guessCount!=1]))
-else:
-    print("You lose! The word was "+word+".")
+    if input('Do you want to play again? Enter "no" to end, otherwise play again! ').lower()=='no':
+        break
+    print()
