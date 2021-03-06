454bbfe0a4728d22f472b62512f6a1f2e938878b
Anthony Grooms <48000635+anthonygrooms01@users.noreply.github.com>
2021-03-06 11:14:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
     Blueprint:
 
     Pick a random word from the word list
-    Ask player to enter a letter
+    Ask the player to enter a letter
     Everytime the player enters a letter that is in the word, reveal that letter
-    Process continues until all letters are revealed or the player ran out of guesses
+    If the letter is not in the word, subtract 1 from the guess count
+    Process continues until all letters are revealed or the player runs out of guesses
 
 '''
 
@@ -12,20 +13,20 @@ import random #import random functionality
 wordsFile = open('Words.txt','r') #open the words file
 lstWords = wordsFile.readlines() #create a list of words
 wordsFile.close() #close the file
-validLetters = 'abcdefghijklmnopqrstuvwxyz'
+validLetters = list('abcdefghijklmnopqrstuvwxyz')
 deadGuyPicture = ['\nO O','\n[.]','\n---','\n | ','\n | ','\n/',' \\'] #create death picture into the deadGuyPicture list
 
 #run the game indefinitely until the user does not want to play again
+#while True: 
+
 while True:
 
     word = lstWords[random.randint(0,len(lstWords)-1)] #pick a random word
     word = word[:-1] #remove the newline from the word
 
     #create the hidden word string
-    displayWord = ''
-    for letter in word:
-        displayWord+='_'
-    
+    displayWord = '_'*len(word)
+
     guessCount = 0
 
     #keep track of the letters the user already entered
@@ -34,14 +35,13 @@ while True:
     #keep asking for input while the player has not uncovered the whole word and they have used less than 7 guesses
     while displayWord != word and guessCount < 7:
 
-        displayWordText = 'Word: '
+        print('Word: ',end='')
 
-        #attach the hidden word string to the hidden word display 
+        #print the display word with spaces in between the characters 
         for letter in displayWord:
-            displayWordText+=letter+' '
-
-        #print the hidden word display
-        print(displayWordText)
+            print(letter+' ',end='')
+        
+        print()
 
         #ask the user for a letter
         while True:
@@ -55,6 +55,8 @@ while True:
             else:
                 print('Not a valid letter\n')
 
+        
+
         #if the letter is in word, edit the hidden word string to display the found letter
         if letter in word:
             for index in range(len(word)):
@@ -67,14 +69,15 @@ while True:
             print()
 
         print()
+            
+
 
     #if the guesscount is less than seven, say the player won, otherwise, say the player lossed
     if guessCount < 7:
-        print('You win! The word is {}. You guessed incorrectly {} {}.'.format(displayWord,guessCount,'time'+('','s')[guessCount!=1]))
+        print('You win! The word is {}. You guessed incorrectly {} {}.'.format(word,guessCount,'time'+('','s')[guessCount!=1]))
     else:
         print('You lose! The word was '+word+'.')
 
     if input('Do you want to play again? Enter "no" to end, otherwise play again! ').lower()=='no':
         break
     print()
-
